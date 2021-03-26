915c1382abb08a16f9bf531f4a2947dc0104e0b9
Adolfo Neto <adolfo.usp@gmail.com>
2021-03-26 12:23:30 -0300

    Now we have limits to the window.

--- lib/projetinho_web/live/page_live.ex
+++ lib/projetinho_web/live/page_live.ex
@@ -1,14 +1,23 @@
 defmodule ProjetinhoWeb.PageLive do
   use ProjetinhoWeb, :live_view
 
-  @colors ["pink", "red", "purple", "green", "blue"]
+  @colors_pt_en [
+    {"rosa", "pink"},
+    {"vermelho", "red"},
+    {"roxo", "purple"},
+    {"verde", "green"},
+    {"branco", "white"},
+    {"azul", "blue"}
+  ]
+
+  @interval 3000
 
   def mount(_params, _session, socket) do
     if connected?(socket) do
-      :timer.send_interval(1000, :tick)
+      :timer.send_interval(@interval, :tick)
     end
 
-    {:ok, assign(socket, x: 0, y: 0, color: "purple")}
+    {:ok, assign(socket, x: 0, y: 0, color: choose_color(), cores: @colors_pt_en)}
   end
 
   def render(assigns) do
@@ -19,36 +28,53 @@ defmodule ProjetinhoWeb.PageLive do
         <rect x="<%= @x * 25 %>" y="<%= @y * 25 %>"
          width="25" height="25" style="fill:<%= @color %>;" />
       </svg>
-      <pre><%= inspect(@color) %></pre>
+      <pre><%= inspect(get_color_name(@color)) %></pre>
     </section>
     """
   end
 
   def handle_info(:tick, socket) do
-    {:noreply, assign(socket, color: Enum.random(@colors))}
+    {:noreply, assign(socket, color: choose_color())}
   end
 
-  def handle_event("keydown", %{"key" => "ArrowRight"}, socket) do
-    {:noreply, update(socket, :x, &(&1 + 1))}
+  defp choose_color() do
+    @colors_pt_en
+    |> Enum.map(fn {_, en} -> en end)
+    |> Enum.random()
+  end
+
+  defp get_color_name(color) do
+    @colors_pt_en
+    |> Enum.find(fn {_pt, en} -> color == en end)
+    |> elem(0)
   end
 
   def handle_event("keydown", %{"key" => "ArrowLeft"}, socket) do
-    {:noreply, update(socket, :x, &(&1 - 1))}
+    {:noreply, update(socket, :x, &left_or_up/1)}
   end
 
-  def handle_event("keydown", %{"key" => "ArrowUp"}, socket) do
-    {:noreply, update(socket, :y, &(&1 - 1))}
+  defp left_or_up(x) when x == 0, do: x
+  defp left_or_up(x), do: x - 1
+
+  def handle_event("keydown", %{"key" => "ArrowRight"}, socket) do
+    {:noreply, update(socket, :x, &right/1)}
   end
+
+  defp right(x) when x == 25, do: x
+  defp right(x), do: x + 1
 
   def handle_event("keydown", %{"key" => "ArrowDown"}, socket) do
-    {:noreply, update(socket, :y, &(&1 + 1))}
+    {:noreply, update(socket, :y, &down/1)}
+  end
+
+  defp down(x) when x == 13, do: x
+  defp down(x), do: x + 1
+
+  def handle_event("keydown", %{"key" => "ArrowUp"}, socket) do
+    {:noreply, update(socket, :y, &left_or_up/1)}
   end
 
   def handle_event("keydown", _key, socket) do
     {:noreply, socket}
   end
-
-
 end
-
-
